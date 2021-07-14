e5bb85c91af2600905b23c12b0ca45a150a2c1ad
josguerrerop <josguerrerop@correo.udistrital.edu.co>
2021-07-14 16:11:00 -0500

    routing done

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,19 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './components/employee/employee.component';
+
 import {FormsModule} from '@angular/forms';
-import { SocketIoComponent } from './components/socket-io/socket-io.component'
+import { AppRoutingModule } from './app-routing.module';
+//import { SocketIoComponent } from './components/socket-io/socket-io.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent,
-    SocketIoComponent
+    EmployeeComponent
+    //AppRoutingModule
+    //SocketIoComponent,
+    //DetailsComponent,
+   // AppRoutingModule
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
