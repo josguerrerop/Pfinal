6abbb34cb6b5f4519d2a12e9656f65079b38105f
josguerrerop <josguerrerop@correo.udistrital.edu.co>
2021-11-20 15:13:19 -0500

    Side-nav

--- myapp/src/app/dashboard/components/wrapper/wrapper.component.ts
+++ myapp/src/app/dashboard/components/wrapper/wrapper.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wrapper.component.css']
 })
 export class WrapperComponent implements OnInit {
-
+   expanded:boolean = false;
+   mode:String=''
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  expander(){
+    if(!this.expanded){
+      this.expanded=true;
+    }else{
+      this.expanded=false;
+      
+    }
+    console.log(this.expanded)
   }
 
 }
